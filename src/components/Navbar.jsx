06bf3b8a338adcda993b75c84fd0ee4bacd3ff99
Faiaz Hossain <faiazhossain10@gmail.com>
2024-03-19 11:35:26 +0600

    feat: navbar feature add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ import { logo2, menu, close } from "../assets";
 import fhIcon from "../assets/fhIcon.jpeg";
 const Navbar = () => {
   const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
   return (
     <nav
       className={`${styles.paddingX} w-full flex-items-center py-5 fixed top-0 z-20 bg-primary`}
@@ -30,6 +31,33 @@ const Navbar = () => {
             Faiaz <span className="sm:block hidden">| Software Engineer</span>
           </p>
         </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+
+        <div className="sm:hidden flex flex-1 justify-end items-center gap-5">
+          <img
+            src={toggle ? close : menu}
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain cursor-pointer"
+            onClick={() => setToggle(!toggle)}
+          />
+          <div
+            className={`${
+              !toggle ? "hidden" : "flex"
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          ></div>
+        </div>
       </div>
     </nav>
   );
