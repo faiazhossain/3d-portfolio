e9d50e90bd8f8225636c6e8bfce860197a653651
Faiaz Hossain <faiazhossain10@gmail.com>
2024-03-20 15:31:29 +0600

    feat: navbar add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+
 import { Link } from "react-router-dom";
 
 import { styles } from "../styles";
 import { navLinks } from "../constants";
 import { logo2, menu, close } from "../assets";
+import { useState } from "react";
 // import fhIcon from "../assets/fhIcon.jpeg";
 const Navbar = () => {
   const [active, setActive] = useState("");
@@ -27,8 +28,9 @@ const Navbar = () => {
             alt="logo"
             className="w-9 h-9 object-contain"
           ></img>
-          <p className="text-white text-[18px] font-bold cursor-pointer">
-            Faiaz <span className="sm:block hidden">| Software Engineer</span>
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+            Faiaz &nbsp;
+            <span className="sm:block hidden">&nbsp;| Software Engineer</span>
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
@@ -56,7 +58,24 @@ const Navbar = () => {
             className={`${
               !toggle ? "hidden" : "flex"
             } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
-          ></div>
+          >
+            <ul className="list-none flex justify-end items-start flex-col gap-4">
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${
+                    active === link.title ? "text-white" : "text-secondary"
+                  } font-poppins font-medium cursor-pointer text-[16px]`}
+                  onClick={() => {
+                    setToggle(!toggle)
+                    setActive(link.title)
+                  }}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
